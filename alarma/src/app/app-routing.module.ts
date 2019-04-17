94faaa895ef72aa326f8138be2bf30031c579177
Paola <torrealbapa@hotmail.com.ar>
2019-04-16 23:44:12 -0300

    agrego alarma
    agrego alarma

---
+++ alarma/src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', loadChildren: './home/home.module#HomePageModule' },
+  { path: 'login', loadChildren: './componentes/login/login.module#LoginPageModule' },
+  { path: 'error', loadChildren: './componentes/error/error.module#ErrorPageModule' },
+  { path: 'usuario', loadChildren: './componentes/usuario/usuario.module#UsuarioPageModule' },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
